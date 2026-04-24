0e068236d0446207d80161aa7033305e60ad6f51
francisyeremiah <francisyeremiah@gmail.com>
2026-04-24 19:32:55 +0300

    data base created and rendering form

---
+++ src/app/write/page.js
@@ -0,0 +1,44 @@
+import { AsyncDatabase } from "promised-sqlite3";
+import postNote from "./postNote";
+
+export default async function WritePage(){
+    async function getUsers(){
+        const db = AsyncDatabase.open("./notes.db");
+        return db.all("SELECT * FROM users");
+    }
+
+    const users = await getUsers();
+
+    return (
+        <div>
+            <fieldset className="note-fieldset">
+                <legend>Write a New Note</legend>
+                <form action={postNote} className="note-form">
+                    <label>From
+                    <select name="from_user">
+                        {users.map((user) => (
+                            <option key={user.id} value={user.id}>
+                                {user.name}
+                            </option>
+                        ))}
+                    </select>
+                    </label>
+                    <label>
+                        To
+                        <select name="to_user">
+                        {users.map((user) => (
+                            <option key={user.id} value={user.id}>
+                                {user.name}
+                            </option>
+                        ))}
+                    </select>
+                    </label>
+                    <label>Note
+                        <textarea name="note" />
+                    </label>
+                    <button type="submit">Save</button>
+                </form>
+            </fieldset>
+        </div>
+    );
+}
